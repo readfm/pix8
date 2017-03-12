299a0e0686c44fa1d0664185cf1cb54ebd8db3a4
mnts <mntask@gmail.com>
2017-03-12 08:19:17 -0600

    Add files via upload

---
+++ cfg.js
@@ -0,0 +1,30 @@
+var Cfg = {
+	//server: 'io.cx/',
+	//server: '127.0.0.1:80/',
+	server: 'io.cx/',
+	//server: '10.0.0.29/',
+	files: 'http://files.mp3gif.com/',
+	thumber: 'http://thumb.pix8.co/',
+	local: true,
+	collection: 'pix8',
+	fixed: true,
+	toBottom: false,
+	drag: {
+		takeOffLimit: 5,
+		dy: 15,
+		dx: 10
+	},
+
+	collector: {
+		minWidth: 60,
+		minHeight: 60,
+		limit: 20
+	},
+
+	gid: '103915987270794097143',
+
+
+	game: {
+		startLimit: 0.3
+	}
+}
